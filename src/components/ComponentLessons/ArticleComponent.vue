d40541881b43d9b04771cc448808dec3365a09ab
rzaw <matijs@lode.family>
2022-05-16 09:44:23 +0300

    Completed lesson (31). Learned about props in Vue.js. You can define prop type, default values and is property required.

---
+++ src/components/ComponentLessons/ArticleComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <h2>{{title}}</h2>
+  <h3>This post has {{likes}} likes</h3>
+  <p>{{isPublished ? 'Public' : 'Draft'}}</p>
+</template>
+
+<script>
+export default {
+    name: 'ArticleComponent',
+    props: {
+        title: {
+          type: String,
+          required: true,
+          default: 'Article name'
+        },
+        likes: Number,
+        isPublished: Boolean
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/components/ComponentLessons/BasicComponentManipulationComponent.vue
+++ src/components/ComponentLessons/BasicComponentManipulationComponent.vue
@@ -5,14 +5,17 @@
   <GreetComponent :firstname="first" :lastname="last" />
   <GreetComponent firstname="Anna" lastname="Lode" />
   <GreetComponent firstname="Teodors" lastname="Lode" />
+  <ArticleComponent title="Hello" :likes="3" :isPublished="true"/>
 </template>
 
 <script>
 import GreetComponent from "./GreetComponent.vue";
+import ArticleComponent from "./ArticleComponent.vue";
 export default {
   name: "BasicComponentManipulationComponent",
   components: {
     GreetComponent,
+    ArticleComponent
   },
   data() {
       return {
